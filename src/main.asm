cdaa3709b3a70578081a24d65cff6d94cd7b1f08
Ryan Garrett <ryan@thepinkhacker.com>
2023-09-19 17:49:54 -0400

    Create game loop

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,6 @@
 .nolist
 #include "includes/ti84pce.inc"
+#define GBUF PlotSScreen
 .list
 
 .org userMem - 2
@@ -7,24 +8,52 @@
 Start:
   call _RunIndicOff
   call _ClrLCDAll
+  call _HomeUp
+  ld hl, StartMessage
+  call _PutS
   call _GetKey
+  jr GameLoop
 Exit:
   call _ClrLCDAll
   call _DrawStatusBar
   call _GrBufClr ; Clears the graph
   set graphDraw, (iy + graphFlags) ; Forces the graph to rerender
+  ld hl, EndMessage
+  call _PutS
+  call _NewLine
   ret
 
 GameLoop:
+  call _ClrLCDAll
+_GameLoop:
   call _GrBufClr
-  ld de, PlotSScreen
-  ;ld hl, Data
+  ld de, GBUF
+  ld hl, Data
   ld bc, 64
   ldir
   call _GrBufCpyV
-  call _GetKey
-  cp kClear
-  jr nz, GameLoop
+  ld a, (Data)
+  inc a
+  ld (Data), a
+  call _GetCSC
+  cp skClear
+  jr nz, _GameLoop
   ret
+
+StartMessage:
+  .db "Hi there, press anything to start...", 0
+
+EndMessage:
+  .db "EXITED", 0
+
+Data:
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
 
 .end
