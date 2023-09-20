0121a120d9dc1a28b410d8db2f1950a8fdf6f9c1
Ryan Garrett <ryan@thepinkhacker.com>
2023-09-20 00:42:08 -0400

    Fill background
    Rainbow effect

--- src/main.asm
+++ src/main.asm
@@ -1,3 +1,5 @@
+.equ vRamSize (vRamEnd - vRam)
+
 .nolist
 #include "includes/ti84pce.inc"
 .list
@@ -13,50 +15,51 @@ Start:
   call _GetKey
   jr GameLoop
 Exit:
+  call _ClrLCDAll
+  call _HomeUp
+  ld hl, EndMessage
+  call _PutS
   ret
 
 GameLoop:
   call _ClrLCDAll
   ld de, vRam
   ld a, lcdHeight
+  ld ix, TestPixelData
 _GameLoop:
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ld hl, Data
-  ld bc, 64
-  ldir
-  ;call _GetCSC
-  ;cp skClear
-  ;jr nz, _GameLoop
-  cp 0
-  dec a
+  inc ix
+  call FillScreen16
+  call _GetCSC
+  cp skClear
   jr nz, _GameLoop
-  call _GetKey
+  ret
+
+FillScreen16:
+; Fill the entire screen with one color.
+;
+; Inputs:
+;   ix = *2-byte rgbb color
+; Output:
+;   hl = vRamEnd
+;   de = vRamEnd
+; Destorys:
+;   Registers:
+;     c
+;     hl
+;     de
+;   Flags:
+;     z
+  ld hl, vRam
+  ld de, vRamEnd
+_FillScreenLoop:
+  ld c, (ix)
+  ld (hl), c
+  inc hl
+  ld c, (ix + 1)
+  ld (hl), c
+  inc hl
+  call _CpHlDe
+  jr nz, _FillScreenLoop
   ret
 
 StartMessage:
@@ -65,14 +68,15 @@ StartMessage:
 EndMessage:
   .db "EXITED", 0
 
-Data:
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
-  .db $01, $23, $45, $67, $89, $AB, $CD, $EF
+TestPixelData:
+; 64 Bytes
+  .dw $F000, $F000, $F000, $F000 ; Red
+  .dw $F000, $F000, $F000, $F000 ; Red
+  .dw $0F00, $0F00, $0F00, $0F00 ; Green
+  .dw $0F00, $0F00, $0F00, $0F00 ; Green
+  .dw $00FF, $00FF, $00FF, $00FF ; Blue
+  .dw $00FF, $00FF, $00FF, $00FF ; Blue
+  .dw $0000, $0000, $0000, $0000 ; Black
+  .dw $FFFF, $FFFF, $FFFF, $FFFF ; White
 
 .end
